d026732401ba33fb9dabb5eb164bc5e0f590c18d
Eduardo Maciel <eduardomacielbr@gmail.com>
2024-12-16 15:47:21 -0300

    chore: dependencies update

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), paraglide({
-        project: "./project.inlang",
-        outdir: "./src/lib/paraglide"
-    })]
+    plugins: [
+        paraglide({
+            project: "./project.inlang",
+            outdir: "./src/lib/paraglide"
+        }),
+        sveltekit()
+    ]
 });
